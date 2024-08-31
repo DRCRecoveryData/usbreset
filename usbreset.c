315a731aec3163716c47062b3589400b38371b70
DRC Lab <85211068+DRCRecoveryData@users.noreply.github.com>
2024-08-31 19:56:25 +0700

    Update usbreset.c

--- usbreset.c
+++ usbreset.c
@@ -1,13 +1,9 @@
-/* usbreset -- send a USB port reset to a USB device */
-
 #include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <errno.h>
 #include <sys/ioctl.h>
-
 #include <linux/usbdevice_fs.h>
-
 
 int main(int argc, char **argv)
 {
@@ -16,25 +12,31 @@ int main(int argc, char **argv)
     int rc;
 
     if (argc != 2) {
-        fprintf(stderr, "Usage: usbreset device-filename\n");
+        fprintf(stderr, "Usage: %s <device-filename>\n", argv[0]);
         return 1;
     }
+
     filename = argv[1];
 
+    // Open the device file
     fd = open(filename, O_WRONLY);
     if (fd < 0) {
-        perror("Error opening output file");
+        perror("Error opening device file");
         return 1;
     }
 
+    // Send the USB reset command
     printf("Resetting USB device %s\n", filename);
     rc = ioctl(fd, USBDEVFS_RESET, 0);
     if (rc < 0) {
-        perror("Error in ioctl");
+        perror("Error resetting USB device");
+        close(fd);
         return 1;
     }
-    printf("Reset successful\n");
 
+    printf("USB reset successful\n");
+
+    // Close the device file
     close(fd);
     return 0;
 }
